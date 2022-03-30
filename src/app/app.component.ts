a66fb44d3f3bf2227290c7578b5789715553b3fe
Carolinaalecrim <carolina.glauseralecrim@gmail.com>
2022-03-29 22:45:44 -0300

    Site parcial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+interface Cliente {
+  id: number,
+  servico: number,
+  cliente: string,
+  contato: string
+}
+enum Servico {
+  'Cabelo',
+  'Barba',
+  'Combo'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'siteBarbearia';
+  title = 'barbearia';
+  url: string = 'http://lucasreno.kinghost.net/barbearia';
+  fila: Cliente[] = [];
+  servicos: typeof Servico = Servico;
+  constructor(
+    public http: HttpClient,
+  ) {
+    this.pegarDados();
+  }
+  ngOnInit() {
+    setInterval(
+      () => {
+        this.pegarDados();
+      }
+      , 10000
+    )
+  }
+  pegarDados() {
+    this.http.get<Cliente[]>(this.url).subscribe(
+      (resposta: Cliente[]) => {
+        this.fila = resposta;
+      }
+    );
+  }
 }
